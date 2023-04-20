191555cbc1c85fdfd88ce36217822b200efa366c
SevaStopAll <sevastopallrh@yandex.ru>
2023-04-20 13:20:02 +0400

    Corrected a little readme Created comments on SQL

--- README.md
+++ README.md
@@ -1,19 +1,35 @@
 # job4j_cinema
 
 ## About
+This a test project for check my knowledge about base web applications.
+The main task is understanding web application architecture principles and simulate 
+a cinema theatre work. User can see all films and can buy a ticket. 
 
-### This a test project for check my knowledge about base web applications.
 
-#### The main task is understanding web application architecture principles and simulate
-#### a cinema theatre work. User can see all films and can buy a ticket. 
 
-#### Stack:  Spring Boot, PostgreSQL, Thymeleaf, Bootstrap, Liquibase, Sql2o. 
+### Features
+### On the main page you can see all necessary links.
+![image](https://user-images.githubusercontent.com/115623776/226090295-61b3a074-30f3-4dbb-a859-50db881f1448.png)
 
-### Launch Project. Launch Main.java in job4j_cinema/src/main/java/job4j_cinema
-
-### Interface ![image](https://user-images.githubusercontent.com/115623776/226090295-61b3a074-30f3-4dbb-a859-50db881f1448.png)
+### In Cine library you can see all films in the theatre.
 ![image](https://user-images.githubusercontent.com/115623776/226090358-2f5dad38-cdf5-4735-8352-30594b6b7f8f.png)
 
+
+#### Stack:
+- **Java 17**
+- **Spring Boot 2.7.10**
+- **PostgreSQL 14**
+- **Thymeleaf**
+- **Bootstrap**
+- **Liquibase**
+- **Sql2o**
+- 
+#### Requirements:
+- **Java 17**
+- **Maven 3.8**
+- **PostgresSQL 14**
+
+### Launch Project. Launch Main.java in job4j_cinema/src/main/java/job4j_cinema
 
 ### Contacts 
 TELEGRAM @vsevolod_prof
--- db/scripts/001_ddl_create_initial_schema.sql
+++ db/scripts/001_ddl_create_initial_schema.sql
@@ -5,11 +5,20 @@ create table files
     path varchar not null unique
 );
 
+comment on table files is 'File table';
+comment on column files.id is 'File id';
+comment on column files.name is 'File name';
+comment on column files.path is 'File path';
+
 create table genres
 (
     id   serial primary key,
     name varchar unique not null
 );
+
+comment on table genres is 'Genre table';
+comment on column genres.id is 'Genre id';
+comment on column genres.name is 'Genre name';
 
 create table films
 (
@@ -23,6 +32,17 @@ create table films
     file_id             int references files (id)  not null
 );
 
+comment on table films is 'Film table';
+comment on column films.id is 'Film id';
+comment on column films.name is 'Film name';
+comment on column films.description is 'Film description';
+comment on column films."year" is 'Film laucnh year';
+comment on column films.genre_id is 'Film genre';
+comment on column films.minimal_age is 'Minimal access age';
+comment on column films.duration_in_minutes is 'Film duration';
+comment on column films.file_id is 'Film poster';
+
+
 create table halls
 (
     id          serial primary key,
@@ -31,6 +51,13 @@ create table halls
     place_count int     not null,
     description varchar not null
 );
+
+comment on table halls is 'Hall table';
+comment on column halls.id is 'Hall id';
+comment on column halls.name is 'Hall name';
+comment on column halls.row_count is 'Hall rows';
+comment on column halls.place_count is 'Hall places in each row';
+comment on column halls.description is 'Hall description';
 
 create table film_sessions
 (
@@ -42,6 +69,14 @@ create table film_sessions
     price      int                       not null
 );
 
+comment on table film_sessions is 'Film Session table';
+comment on column film_sessions.id is 'Session id';
+comment on column film_sessions.film_id is 'Session film';
+comment on column film_sessions.hall_id is 'Session hall';
+comment on column film_sessions.start_time is 'Session start time';
+comment on column film_sessions.end_time is 'Session end time';
+comment on column film_sessions.price is 'Session ticket price';
+
 create table users
 (
     id        serial primary key,
@@ -49,6 +84,12 @@ create table users
     email     varchar unique not null,
     password  varchar        not null
 );
+
+comment on table users is 'User table';
+comment on column users.id is 'User id';
+comment on column users.name is 'User name';
+comment on column users.email is 'User email';
+comment on column users.password is 'User password';
 
 create table tickets
 (
@@ -59,3 +100,10 @@ create table tickets
     user_id      int                               not null,
     unique (session_id, row_number, place_number)
 );
+
+comment on table tickets is 'Ticket table';
+comment on column tickets.id is 'Ticket id';
+comment on column tickets.session_id is 'Film session';
+comment on column tickets.row_number is 'Row number';
+comment on column tickets.place_number is 'Place number';
+comment on column tickets.user_id is 'User, bought this ticket';
